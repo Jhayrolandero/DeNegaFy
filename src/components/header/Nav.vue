f21f0ea7e85b00f040961262911bac14ceb363af
Cjay Landero <xjaylandero@gmail.com>
2024-01-14 18:34:36 +0800

    Added FAQ

--- src/components/header/Nav.vue
+++ src/components/header/Nav.vue
@@ -1,6 +1,7 @@
 <template>
     <nav class="flex gap-2 text-2xl max-sm:flex-col">
         <button class="transition hover:font-semibold duration-150" @click="scrollTo('home')">Home |</button>
+        <button class="hover:font-semibold" @click="scrollTo('faq')">FAQ |</button>
         <button class="hover:font-semibold" @click="scrollTo('about')">About |</button>
         <button class="hover:font-semibold" @click="">Demo</button>
     </nav>
