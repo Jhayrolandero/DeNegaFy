2eceb33418a6126409fc734cd89f6d6cdf58c237
Cjay Landero <xjaylandero@gmail.com>
2024-01-18 19:53:27 +0800

    Prototype for Demo Section

--- src/components/header/Nav.vue
+++ src/components/header/Nav.vue
@@ -3,8 +3,9 @@
         <button class="transition hover:font-semibold duration-150" @click="scrollTo('home')">Home |</button>
         <button class="hover:font-semibold" @click="scrollTo('faq')">FAQ |</button>
         <button class="hover:font-semibold" @click="scrollTo('about')">About |</button>
-        <button class="hover:font-semibold" @click="">Demo |</button>
-        <button class="hover:font-semibold" @click="scrollTo('contribution')">Contribution</button>
+        <button class="hover:font-semibold" @click="scrollTo('demo')">Demo |</button>
+        <button class="hover:font-semibold" @click="scrollTo('contribution')">Contribution |</button>
+        <button class="hover:font-semibold" @click="scrollTo('contribution')">Documentation</button>
     </nav>
 </template>
 
---
+++ src/components/sections/demo/Demo.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div>
+        <input type="text" :value="activity" class="text-white bg-black border-2 border-white px-3 py-2">
+        <button @click="getActivity" class="text-white border-2 border-white px-3 py-2">DeNegaFy</button>
+    </div>
+</template>
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const activity = ref('')
+
+async function getActivity() {
+    try {
+        const response = await axios.get('http://localhost:8080/api/v1/activity');
+        activity.value = response.data.activityTitle
+    } catch (error) {
+        console.error(error);
+    }
+}
+</script>
